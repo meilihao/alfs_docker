3d46d8917498421b8b790e16574046d33a9207da
meilihao <563278383@qq.com>
2020-08-20 11:55:44 +0800

    + run-prepare.sh build framework

---
+++ scripts/prepare/binutils.sh
@@ -0,0 +1,7 @@
+#!/usr/bin/env bash
+set -e
+echo -e "\n\n+++ start binutils.sh +++\n\n"
+
+
+
+echo -e "+++ done binutils.sh +++\n\n"
--- scripts/prepare/run-prepare.sh
+++ scripts/prepare/run-prepare.sh
@@ -1,7 +1,12 @@
 #!/usr/bin/env bash
 set -e
+
+LFS_Script_Prepare=/lfs/scripts/prepare
+
 echo "--- start run-prepare.sh ---"
 
-
+${LFS_Script_Prepare}/binutils.sh
 
 echo -e "--- done run-prepare.sh ---\n\n"
+
+unset  LFS_Script_Prepare
