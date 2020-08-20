339e4a9ce04f355ceb2e95d41871bbbe54c22c7f
meilihao <563278383@qq.com>
2020-08-20 10:33:27 +0800

    f avoid to rebuild lfs when "docker exec -it <container_id> bash"

--- scripts/run-all.sh
+++ scripts/run-all.sh
@@ -1,6 +1,17 @@
 #!/usr/bin/env bash
 set -e
 
+# avoid to rebuild lfs when "docker exec -it <container_id> bash"
+LFS_Build_Done=~/.lfs_build_done
+
+if [ -f ${LFS_Build_Done} ]; then
+    echo "--- lfs build already done at  ---"
+    cat ${LFS_Build_Done} >&1
+    exit 0
+fi
+
+echo `date` > ${LFS_Build_Done}
+
 echo "--- print env ---"
 env
 echo -e "--- print env done---\n\n"
