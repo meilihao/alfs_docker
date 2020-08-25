a65fadf8ab3f706f6fa7c34a716246611b311773
meilihao <563278383@qq.com>
2020-08-25 13:11:31 +0800

    .

--- scripts/build/creating-essential-files-and-symlinks.sh
+++ scripts/build/creating-essential-files-and-symlinks.sh
@@ -58,6 +58,7 @@ nogroup:x:99:
 users:x:999:
 EOF
 
+# in docker, `$(ls -n $(tty) | cut -d" " -f3)`=0
 echo "tester:x:101:101::/home/tester:/bin/bash" >> /etc/passwd
 echo "tester:x:101:" >> /etc/group
 install -o tester -d /home/tester
