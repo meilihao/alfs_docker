7c84f9f46e07ac4f5c54265942a5113091c850b0
meilihao <563278383@qq.com>
2020-08-26 09:55:33 +0800

    + the-end

--- scripts/build/run-build-in-chroot-system-config.sh
+++ scripts/build/run-build-in-chroot-system-config.sh
@@ -6,5 +6,6 @@ ${LFSRoot}/scripts/build/system-config.sh
 ${LFSRoot}/scripts/build/cpio.sh
 ${LFSRoot}/scripts/build/lz4.sh
 ${LFSRoot}/scripts/build/kernel.sh
+${LFSRoot}/scripts/build/the-end.sh
 
 echo -e "--- done run-build-in-chroot-system-config.sh in chroot ---\n\n"
---
+++ scripts/build/the-end.sh
@@ -0,0 +1,22 @@
+#!/usr/bin/env bash
+set -e
+echo -e "--- start the-end.sh in chroot ---\n\n"
+
+echo 10.0-systemd-rc1 > /etc/lfs-release
+
+cat > /etc/lsb-release << "EOF"
+DISTRIB_ID="Linux From Scratch"
+DISTRIB_RELEASE="10.0-systemd-rc1"
+DISTRIB_CODENAME="BigBang"
+DISTRIB_DESCRIPTION="Linux From Scratch for fun"
+EOF
+
+cat > /etc/os-release << "EOF"
+NAME="Linux From Scratch"
+VERSION="10.0-systemd-rc1"
+ID=lfs
+PRETTY_NAME="Linux From Scratch 10.0-systemd-rc1"
+VERSION_CODENAME="Linux From Scratch for fun"
+EOF
+
+echo -e "--- done the-end.sh in chroot ---\n\n"
