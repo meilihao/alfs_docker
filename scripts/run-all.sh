31ed7305fe2b91aeebb8c0018a9ab3ea53c7751e
meilihao <563278383@qq.com>
2020-09-02 21:15:25 +0800

    fs layout follow ubuntu20.04/deepin v20

--- scripts/run-all.sh
+++ scripts/run-all.sh
@@ -14,7 +14,14 @@ echo `date` > ${LFS_Build_Done}
 
 echo -e "--- start build lfs ---\n\n"
 
-mkdir -pv ${LFS}/{bin,etc,lib,lib64,sbin,usr,var,tools}
+mkdir -pv ${LFS}/{usr/bin,usr/sbin,usr/lib,usr/lib32,usr/lib64,usr/libx32,etc,var,tools}
+
+ln -sfv usr/bin     $LFS/bin
+ln -sfv usr/sbin    $LFS/sbin
+ln -sfv usr/lib     $LFS/lib
+ln -sfv usr/lib32   $LFS/lib32
+ln -sfv usr/lib64   $LFS/lib64
+ln -sfv usr/libx32  $LFS/libx32
 
 # logs for debug log when building and user tester can write log
 mkdir -pv ${LFS}/logs
