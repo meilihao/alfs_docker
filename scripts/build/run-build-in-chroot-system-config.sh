d04077924f2c34dee1ceea480790ad29ea061957
meilihao <563278383@qq.com>
2020-09-02 08:30:12 +0800

    - clean doc after all done

--- scripts/build/run-build-in-chroot-system-config.sh
+++ scripts/build/run-build-in-chroot-system-config.sh
@@ -7,5 +7,6 @@ ${LFSRoot}/scripts/build/cpio.sh
 ${LFSRoot}/scripts/build/lz4.sh
 ${LFSRoot}/scripts/build/kernel.sh
 ${LFSRoot}/scripts/build/the-end.sh
+${LFSRoot}/scripts/build/cleanup.sh
 
 echo -e "--- done run-build-in-chroot-system-config.sh in chroot ---\n\n"
