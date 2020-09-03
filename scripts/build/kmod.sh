78b3e6248cc774d6da2c1f5ea764c6a13d1faaef
meilihao <563278383@qq.com>
2020-09-03 14:45:50 +0800

    f build failed after change fs layout

--- scripts/build/kmod.sh
+++ scripts/build/kmod.sh
@@ -1,6 +1,18 @@
 #!/usr/bin/env bash
 set -e
 
+# when --with-rootlibdir=/lib get error:
+# if test "/usr/lib" != "/lib"; then \
+# 	/bin/mkdir -p /lib && \
+# 	so_img_name=$(readlink /usr/lib/libkmod.so) && \
+# 	so_img_rel_target_prefix=$(echo /usr/lib | sed 's,\(^/\|\)[^/][^/]*,..,g') && \
+# 	ln -sf $so_img_rel_target_prefix/lib/$so_img_name /usr/lib/libkmod.so && \
+# 	mv /usr/lib/libkmod.so.* /lib; \
+# fi
+# mv: '/usr/lib/libkmod.so.2' and '/lib/libkmod.so.2' are the same file
+# mv: '/usr/lib/libkmod.so.2.3.5' and '/lib/libkmod.so.2.3.5' are the same file
+# make[3]: *** [Makefile:3026: install-exec-hook] Error 1
+
 echo -e "\n\n+++ start kmod.sh +++\n\n"
 
 BuildDir=`mktemp -d --suffix ".kmod"`
@@ -13,7 +25,7 @@ cd ${BuildDir} && \
 ./configure --prefix=/usr          \
             --bindir=/bin          \
             --sysconfdir=/etc      \
-            --with-rootlibdir=/lib \
+            --with-rootlibdir=/usr/lib   \
             --with-xz              \
             --with-zlib               && \
 make                                  && \
